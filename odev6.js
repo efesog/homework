3e6836b78bf01c3ccfadc5b092bad8958c549783
efesog <eltkoko@yahoo.com>
2023-01-14 20:21:37 +0300

    a

---
+++ odev6.js
@@ -0,0 +1,13 @@
+let acik = document.getElementById('acik')
+let kapali = document.getElementById('kapali')
+let lamba = document.getElementById('lamba')
+
+
+  acik.addEventListener('mouseover', () => {
+  lamba.src= 'lamba_acik.jpg'
+} )
+
+kapali.addEventListener('mouseover', () => {
+    lamba.src = 'lamba_kapali.jpg'
+  } )
+  
